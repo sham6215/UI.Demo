6a8803a1b58d13758fffbb74684c7de2202c4d48
Alexander Shamray <sham6215@yahoo.com>
2017-11-15 17:04:35 +0200

    DataGrid project is added.

---
+++ DataGrid/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using DataGrid.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataGrid
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Order> Orders { get; set; }
+
+        public MainWindow()
+        {
+            Init();
+            InitializeComponent();
+        }
+
+        private void Init()
+        {
+            Orders = new List<Order>() {
+                new Order() { Id = 1, Guid = "Guid1", Name = "Name1", Reference = "Ref1" },
+                new Order() { Id = 2, Guid = "Guid2", Name = "Name2", Reference = "Ref2" },
+                new Order() { Id = 3, Guid = "Guid3", Name = "Name3", Reference = "Ref3" },
+                new Order() { Id = 4, Guid = "Guid4", Name = "Name4", Reference = "Ref4" },
+                new Order() { Id = 5, Guid = "Guid5", Name = "Name5", Reference = "Ref5" },
+            };
+        }
+    }
+}
---
+++ DataGrid/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataGrid.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Guid { get; set; }
+        public string Name { get; set; }
+        public string Reference { get; set; }
+    }
+}
